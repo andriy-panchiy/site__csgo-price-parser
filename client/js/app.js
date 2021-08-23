604bd175514111fd3160dea89b24dd42029770ae
AndrewCrook <7helfs@gmail.com>
2021-08-23 16:42:07 +0300

    Создал фильтр для сравнения площадок
    Создал 2 поля для выбора сайтов у которых я буду сравнивать цены, и выводить названия предметов этих сайтов в таблицу, указывая цену на 1 и 2 площадке, а так же разницу в %, статус предметов на обеих площадках. Фильтры оформил в стиле dropdown меню, созданном на bootstrap

--- client/js/app.js
+++ client/js/app.js
@@ -1,16 +1,29 @@
-"use strict";
+'use strict';
 
 (async function () {
   const app = angular.module('parserApp', [])
-  app.controller('parserController', async function ($scope) {
+  app.controller('parserController', async function ($scope, $http) {
     const localAddress = 'http://127.0.0.1:3000'
-
-    $scope.settings = await fetch(localAddress + '/settings').then(r => r.json());
-    $scope.options = [];
-
-
-    console.dir($scope.options)
-
-  })
+    $http.get(localAddress + '/settings').then(response => {
+      $scope.settings = response.data;
+      $scope.sites = $scope.settings.map(v => v.site);
+      $scope.options = {
+        dropdowns: [
+          { id: 0, title: 'First service', options: $scope.sites, active_index: 0 },
+          { id: 1, title: 'Second service', options: $scope.sites, active_index: 1 },
+          { id: 2, title: 'Unstable1', options: ['ON', 'OFF'], active_index: 0 },
+          { id: 3, title: 'Unstable2', options: ['ON', 'OFF'], active_index: 0 }
+        ],
+      }
+      $scope.items = [{
+        item_name: 'none',
+        price_1: 'none',
+        price_2: 'none',
+        diff: 'none',
+        status_1: 'none',
+        status_2: 'none'
+      }]
+    });
+  });
 
 })()
