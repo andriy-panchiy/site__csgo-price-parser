06e1fde88f19da01c151f81a0cd6ace339971953
AndrewCrook <7helfs@gmail.com>
2021-08-25 03:38:16 +0300

    Удалил публичный ключ

--- server/plugin/mongodb.js
+++ server/plugin/mongodb.js
@@ -1,9 +1,10 @@
 const fastifyPlugin = require('fastify-plugin')
+const api = require('./api.js')
 
 async function dbConnector (fastify, options) {
   console.log(options)
   fastify.register(require('fastify-mongodb'), {
-    url: 'mongodb+srv://admin:sQBSkNKj4Jmu2tbT@cluster0.fntga.mongodb.net/test'
+    url: api.mongodb
   })
 }
 
