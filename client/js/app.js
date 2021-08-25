bdddd11d7280896772787fa517765b6ed2cc71d7
AndrewCrook <7helfs@gmail.com>
2021-08-25 20:53:40 +0300

    Добавил пагинацию и фильтр поиска по имени
    Добавил поле для поиска вещей по имени, добавил кнопки переключения страниц с отображением количества страниц, а также фильтр в котором можно выбрать количество отображаемых предметов на странице

--- client/js/app.js
+++ client/js/app.js
@@ -2,7 +2,7 @@
 
 (async function () {
   const app = angular.module('parserApp', [])
-  app.controller('parserController', async function ($scope, $http) {
+  app.controller('parserController', async function ($scope, $http, $filter) {
     const localAddress = 'http://127.0.0.1:3000'
     $http.get(localAddress + '/settings').then(response => {
       $scope.settings = response.data;
@@ -15,8 +15,29 @@
           { id: 3, title: 'Unstable2', options: ['ON', 'OFF'], active_index: 0 }
         ]
       }
-      $scope.orderType = 'diff'
-      $scope.orderReverse = false
+      $scope.settings = {
+        active_sites: [
+          null, null
+        ],
+        alerts: {
+          success: null,
+          info: null,
+          warning: null,
+          error: null
+        },
+        orderType: 'diff',
+        orderReverse: false,
+        currentPage: 0,
+        min_price_0: 0,
+        max_price_0: 5000,
+        min_price_1: 0,
+        max_price_1: 5000,
+        min_diff: -1000,
+        max_diff: 1000,
+        maxButton: 5,
+        pageSize: 10,
+        search: ''
+      }
       $scope.items = [{
         item_name: 'none',
         price_0: 'none',
@@ -26,18 +47,28 @@
         status_1: 'none'
       }];
 
+      $scope.itemsPerPage = angular.isDefined($scope.itemsPerPage) ? $scope.itemsPerPage : [...Array($scope.settings.maxButton).keys()].map(number => (number+1)*10);
+
+      $scope.getData = () => {
+        return $scope.items.filter((item) => {
+          let by_name = item.item_name.indexOf($scope.settings.search) != -1;
+          let by_price_0 = item.price_0 >= $scope.settings.min_price_0 && item.price_0 <= $scope.settings.max_price_0;
+          let by_price_1 = item.price_1 >= $scope.settings.min_price_1 && item.price_1 <= $scope.settings.max_price_1;
+          let by_diff = item.diff >= $scope.settings.min_diff && item.diff <= $scope.settings.max_diff;
+          return by_name && by_price_0 && by_price_1 && by_diff;
+        })
+      }
+      $scope.numberOfPages = () => Math.ceil($scope.getData().length/$scope.settings.pageSize);
       $scope.upload_service = async () => {
-        $scope.settings = {
-          active_sites: [
-            $scope.options.dropdowns[0].options[$scope.options.dropdowns[0].active_index],
-            $scope.options.dropdowns[1].options[$scope.options.dropdowns[1].active_index]
-          ],
-          alerts: {
-            success: null,
-            info: null,
-            warning: null,
-            error: null
-          }
+        $scope.settings.active_sites = [
+          $scope.options.dropdowns[0].options[$scope.options.dropdowns[0].active_index],
+          $scope.options.dropdowns[1].options[$scope.options.dropdowns[1].active_index]
+        ]
+        $scope.settings.alerts = {
+          success: null,
+          info: null,
+          warning: null,
+          error: null
         }
         if ($scope.settings.active_sites[0] === $scope.settings.active_sites[1]) {
           $scope.settings.alerts.error = 'The choice of the first service corresponds to the choice of the second service, please change one of the selected services';
@@ -70,7 +101,7 @@
           }
           const items = [];
           const names = Object.keys($scope.sites_bases.base_0.base.prices);
-          const diff = (price_0, price_1) => price_0 ? +(((price_0 - price_1) / price_0) * 100).toFixed(2) : 0;
+          const diff = (price_0, price_1) => price_1 ? +(((price_1 - price_0) / price_1) * 100).toFixed(2) : 0;
           const status = (base, name) => {
             let res = 'none';
             if (base.overstock && base.overstock[name]) res = 'overstock';
@@ -102,14 +133,14 @@
         $scope.upload_service();
       }
       $scope.changeSortType = (orderType) => {
-        if ($scope.orderType == orderType) {
-          $scope.orderReverse = !$scope.orderReverse
+        if ($scope.settings.orderType == orderType) {
+          $scope.settings.orderReverse = !$scope.settings.orderReverse
         } else {
-          $scope.orderType = orderType
+          $scope.settings.orderType = orderType
         }
       }
-      $scope.isSortType = (orderType) => $scope.orderType == orderType
-      $scope.isOrderedReverse = () => !$scope.orderReverse
+      $scope.isSortType = (orderType) => $scope.settings.orderType == orderType
+      $scope.isOrderedReverse = () => !$scope.settings.orderReverse
 
     });
   });
@@ -118,6 +149,12 @@
     return function (items, property, min, max) {
       // console.log(items, property, min, max);
       return items.filter((item) => item[property] >= min && item[property] <= max)
+    }
+  });
+  app.filter('startFrom', function() {
+    return function(input, start) {
+        start = +start; //parse to int
+        return input.slice(start);
     }
   });
 
--- server/plugin/mongodb.js
+++ server/plugin/mongodb.js
@@ -1,10 +1,10 @@
 const fastifyPlugin = require('fastify-plugin')
-const api = require('./api.js')
+const secrets = require('./api.js')
 
 async function dbConnector (fastify, options) {
   console.log(options)
   fastify.register(require('fastify-mongodb'), {
-    url: api.mongodb
+    url: secrets.mongodb
   })
 }
 
